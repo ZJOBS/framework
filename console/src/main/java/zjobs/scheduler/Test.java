b4da13a2b9879938a57b49b6187ca4cf47f03dda
jie zhang <zhangjiexyx@icloud.com>
2018-06-21 15:17:15 +0800

    no message

---
+++ console/src/main/java/zjobs/scheduler/Test.java
@@ -0,0 +1,13 @@
+package zjobs.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test {
+
+//    @Scheduled(cron = "0/10 * * * * ? ")
+//    public void run() {
+//        System.out.println("执行定时任务");
+//    }
+}
